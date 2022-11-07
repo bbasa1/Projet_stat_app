e1db65ea05a75fffdbc454a5f3156025c8308f51
bbasa1 <benjamin.basa@ensae.fr>
2022-11-07 17:47:37 +0100

    Inner_et_full_outer_join_1998_1999

--- 1_constitution_base.R
+++ 1_constitution_base.R
@@ -2,6 +2,56 @@ library(readr)
 library(data.table)
 
 
+############## PARTIE 0 : POUR COMPRENDRE LE MERGE DE DATA.TABLE #######################
+set.seed(42)
+big <- data.table( 
+  id = LETTERS[2:11],
+  a = sample(101:105, 10, replace = TRUE),
+  b = sample(200:300, 10)
+)
+small <- data.table(
+  id = LETTERS[1:5],
+  y = sample(1:5, 5, replace = TRUE),
+  z = sample(10:20, 5) 
+)
+small
+big
+
+
+
+### Inner join, par deux méthodes
+small[big, on = .(id), nomatch = NULL]
+
+result <- merge(small, big,
+                all.x = FALSE, 
+                all.y = FALSE, 
+                by = 'id', 
+                allow.cartesian=TRUE)
+result
+inner <- 4
+
+# Ouf les deux méthodes donnent bien la MEME chose !
+
+### Outer join
+
+result <- merge(small, big,
+                all.x = TRUE, 
+                all.y = TRUE, 
+                by = 'id', 
+                allow.cartesian=TRUE)
+
+result[is.na(y) | is.na(a)]
+outer <- 7
+# Ouf ça semble bien fonctionner !
+
+2*inner + outer
+nrow(small) + nrow(big)
+
+
+
+
+
+######## PARTIE 1 : Importation et deux types de merge : inner ou full-outer des bases Françaises 1998 et 1999 ###########
 # Pour $merge$ deux bases sur deux années consécutives en France
 # 
 # ## Explications :
@@ -18,114 +68,63 @@ library(data.table)
 
 
 
-
-# Etape 1 : Ouvrir les données
-
-# path <- "C:/Users/Benjamin/Desktop/Ensae/Projet_statapp/Repo_codes/Data/YearlyFiles_1998_2018"
-# 
-# 
-# # data <- read.table(unz(paste(path,"FR_YEAR_1998_onwards.zip"), "FR2018_y.csv"), nrows=10, header=T, quote="\"", sep=",")
-# 
-# data <- readr::read_csv(file = unzip(paste(path, 'FR_YEAR_1998_onwards.zip', sep="/"), "FR2018_y.csv"),
-#                         show_col_types = FALSE)
-# 
-
-
-
-
-# path <- "C:/Users/Benjamin/Desktop/Ensae/Projet_statapp/Repo_codes/Data/YearlyFiles_1998_2018/FR2015_y.csv"
-# 
-# 
-# data_fr <- read_csv(path, 
-#                     locale = locale(encoding ="UTF-8"),
-#                     # col_types = cols(com = col_character(),
-#                     #                  reg = col_integer()),
-#                     show_col_types = FALSE
-# )
-# 
-# data_fr
-
-
+# Importation
 liste_annees <- 1998:2018
 
 liste_pathes <- paste("C:/Users/Benjamin/Desktop/Ensae/Projet_statapp/Repo_codes/Data/YearlyFiles_1998_2018/FR", liste_annees, sep = "")
 liste_pathes_2 <- paste(liste_pathes, "y.csv", sep = "_")
 
 
-data_concat <- read_csv(liste_pathes_2[1], 
+data_1998 <- read_csv(liste_pathes_2[1], 
                         locale = locale(encoding ="UTF-8"),
                         show_col_types = FALSE
 )
-data_concat <- as.data.table(data_concat)
+data_1998 <- as.data.table(data_1998)
 
 
 path <- liste_pathes_2[2]
-data_loc <- read_csv(path, 
+data_1999 <- read_csv(path, 
                      locale = locale(encoding ="UTF-8"),
                      show_col_types = FALSE
 )
-data_loc <- as.data.table(data_loc)
+data_1999 <- as.data.table(data_1999)
 
 
 suffixe_ajoute <- paste("_", as.character(1998 + 2), sep = "")
 
 
+# Deux types de jointures
+faire_outer_join <- FALSE
 
-data_concat2 <- merge(data_concat, data_loc,
-                      all.x = FALSE, 
-                      all.y = FALSE, 
-                      by = c('QHHNUM', 'HHSEQNUM'), 
-                      allow.cartesian=TRUE, 
-                      suffixes = c("", suffixe_ajoute))
+if(faire_outer_join == FALSE){
+  data_merged <- merge(data_1998, data_1999,
+                        all.x = FALSE, 
+                        all.y = FALSE, 
+                        by = c('QHHNUM', 'HHSEQNUM'), 
+                        allow.cartesian=TRUE, 
+                        suffixes = c("", suffixe_ajoute))
+}else{
+  data_merged <- merge(data_1998, data_1999,
+                        all.x = TRUE, 
+                        all.y = TRUE, 
+                        by = c('QHHNUM', 'HHSEQNUM'), 
+                        allow.cartesian=TRUE, 
+                        suffixes = c("", suffixe_ajoute))
+  data_merged <- data_merged[is.na(SEX_2000) | is.na(SEX)]
+  
+}
 
-data_concat2
+# Quelques vérifs
 
+data_merged
 
-df_loc <- data_loc[QHHNUM == 'Q1003100']
-df_loc
+nrow(data_merged)
 
-###########################################
-# 
-# 
-# for (path in liste_pathes_2[c(-1)]){
-#   data_loc <- read_csv(path, 
-#                        locale = locale(encoding ="UTF-8"),
-#                        show_col_types = FALSE
-#   )
-#   data_loc <- as.data.table(data_loc)
-#   
-#   
-#   data_concat <- merge(data_concat, data_loc, all = TRUE, by = 'QHHNUM')
-#   
-#   # data_concat <- rbindlist(list(data_concat, data_loc))    
-# }
-# 
-# 
-# data_concat_dt <- as.data.table(data_concat)
+View(data_merged[QHHNUM == "Q1003025"])
+View(data_merged[QHHNUM == "Q1001192"])
 
+inner <- 113648
+outer<- 137931
 
-###########################################
-
-# 
-# for (year in c(2010,2011,2012,2013,2014,2015)){
-#   print(paste("The year is", year))
-# }
-# 
-# 
-# data_fr <- read_csv(liste_pathes_2[1], 
-#                      locale = locale(encoding ="UTF-8"),
-#                     show_col_types = FALSE
-#                      )
-# data_fr_dt <- as.data.table(data_fr)
-# 
-# data_fr2 <- read_csv(liste_pathes_2[2], 
-#                      locale = locale(encoding ="UTF-8"),
-#                     show_col_types = FALSE
-#                      )
-# data_fr_dt2 <- as.data.table(data_fr2)
-# 
-# 
-# 
-# data_concat <- rbindlist(list(data_fr_dt, data_fr_dt2))    
-# data_concat                                  
-
+2*inner + outer
+nrow(data_1998) + nrow(data_1999)
