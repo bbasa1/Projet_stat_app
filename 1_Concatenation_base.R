853ef6966d0feacefcd0ea60bb98a5051b2a8337
bbasa1 <benjamin.basa@ensae.fr>
2022-11-17 11:11:19 +0100

    Selection_colonnes_Henri

--- 1_Concatenation_base.R
+++ 1_Concatenation_base.R
@@ -70,12 +70,11 @@ df_plot <- df_plot[ , annee := as.character(annee)]
 
 
 ggplot(data = df_plot) + 
-  geom_bar(aes(x = annee, y = population_1), stat="identity") + 
+  geom_bar(aes(x = annee, y = population), stat="identity") + 
   labs(title="Longueur de la table après concaténation jusqu'à l'année x",
        x="Année",
        y="Population") + 
-  scale_y_continuous(labels = function(y) format(y, scientific = FALSE)) +
-  coord_flip()
+  scale_y_continuous(labels = function(y) format(y, scientific = FALSE))
 
 
 
@@ -83,31 +82,49 @@ ggplot(data = df_plot) +
 
 
 
+############# SELECTION VARIABLES ######################
+liste_variables <- c('QHHNUM', #Identifiant ménage
+                     'COEFFY', #Yearly weighting factor,
+                     'COEFFH', #Yearly weighting factor of the sample for household characteristics
+                     'COUNTRY',
+                     'SEX',
+                     'YEAR',
+                     'AGE',
+                     'YSTARTWK', # Year in which person started working for this employer or as self-employed. 9999 not applicable
+                    'ILOSTAT', # ILO working status. 1 = employed. 2 = Unemployed. 3 = inactive. 4 = Compulsory military service. 5 = persons < 15 years old
+                    'WSTATOR', # Labor status during the reference week (for aged >15 years). 1 = worked at least 1 hours. 2 =  Vacances/service militaire/... 3 = licencicé. 4 = Service militaire ou civique. 5 = Autre.
+                    'WANTWORK', # Willingness to work for person not seeking employment. 1 = but would nevertheless like to have work. 2 = does not want to have work. 9 = not applicable
+                    'WISHMORE', # Wish to work usually more than the current number of hours. 0 = No. 1 = Yes
+                    'AVAILBLE', # Availability to start working within two weeks if work were found now. 1 = Could start to work immediately. 2 = could not start
+                    'SEEKWORK', # Seeking employment during previous four weeks. 1 = Person has already found a job which will start within a period of at most 3 months. 2 = Person has already found a job which will start in more than 3 months. 3 = Person is not seeking employment and has not found
+                                # any job to start late. 4 = Person is seeking employment
+                    'SEEKREAS', # Reasons for not searching an employment because. 1 = awaiting recall to work (persons on lay-off). 2 = of own illness or disability. 3 = looking after children or incapacitated adults (from 2006). 4 = of other personal or family responsibilities. 5 = of education or training. 6 = of retirement. 7 = of belief that no work is available. 8 = of other reasons
+                    'DEGURBA', # Degree of urbanisation. 1 = Densely. 2 = intermediate. 3 = rural
+                    'STAPRO', #Professional status. 1 = Self-employed with employees. 2 = Self-employed without employees. 3 = Employee. 4 = Family worker. 
+                    'FTPT', # 1 = Full-time. 2 = Part-time job.
+                    'ISCO3D',
+                    'TEMP', # 1 = CDI. 2 = CDD
+                    'TEMPDUR', # Total duration of temporary job or work contract of limited duration. 1 = Less than one month. 2 =1 to 3 months. 3 = 4 to 6 months. 4 = 7 to 12 months = 17. 5 = 13 to 18 months. 6 = 19 to 24 months. 7 = 25 to 36 months. 8 = More than 3 years
+                    'FTPTREAS', # Reasons for the part-time work. 1 = Person is undergoing school education or training. 2 = Of own illness or disability. 3 =Looking after children or incapacitated adults. 4 = Other family or personal reasons (from 2006). 5 = Person could not find a full-time job. 6 = Other
+                    'HWWISH', # Number of hours that the person would like to work in total
+                    'HWUSUAL', # Number of hours per week usually worked in the main job
+                    'HWACTUAL', # Number of hours actually worked during the reference week in the main job
+                    'HWACTUA2', # Number of hours actually worked during the reference week in the second job
+                    'INCDECIL', # Monthly (take home) pay from main job
+                    'NEEDCARE', # Person is not searching for a job or is working part time because. 1 = Suitable care services for children are not available or affordable. 2 = Suitable care services for ill, disabled, elderly are not available or affordable. 3 = Suitable care services for both children and ill, disabled and elderly are not available or affordable.. 4 = Care facilities do not influence decision for working part time or not searching for a job
+                    'HATLEV1D', # Level of education. L = low. M = Medium. H = High
+                    'HATFIELD', #Field of education. Different codes before and after 2003
+                    'HHNBCH2', # Number of children [0,2] years in the household
+                    'HHNBCH5', # Number of children  [3,5] years in the household
+                    'HHNBCH8', # Number of children [6,8] years in the household
+                    'HHNBCH11', # Number of children [9,11] years in the household
+                    'HHNBCH14' # Number of children [12,14] years in the household
+                    )
+
+  data_merged <- data_merged[,..liste_variables]
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-select(HHNUM,COEFF,COUNTRY,SEX,YEAR,AGE,YSTARTWK,
-       ILOSTAT,WSTATOR,WANTWORK,WISHMORE,AVAILBLE,SEEKWORK,SEEKREAS,DEGURBA,STAPRO,FTPT,
-       ISCO3D,TEMP,TEMPDUR,FTPTREAS,HWWISH,HWUSUAL,HWACTUAL,HWACTUA2,INCDECIL,
-       NEEDCARE,HATLEV1D,HATFIELD,
-       HHNBCH2,HHNBCH5,HHNBCH8,HHNBCH11,HHNBCH14)
