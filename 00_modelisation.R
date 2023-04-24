748c4fcfcede900369ed84c0bfb91ce54f3e923b
bbasa1 <benjamin.basa@ensae.fr>
2023-04-24 22:34:21 +0200

    Modif_mode

--- 00_modelisation.R
+++ 00_modelisation.R
@@ -17,15 +17,21 @@ repgen <- "C:/Users/Benjamin/Desktop/Ensae/Projet_statapp"#BB
 
 ### Ensemble des paramètres pour faire tourner au lundi 24 avril : 
 kmeans_sur_proj <- FALSE
-methode <- "euclidean"
+methode <- "manhattan"
 #manhattan = distance L1
 #euclidean = distance L2
 #Chebychev = disrance L^infini
+nb_clusters <- 6
 
+
+faire_traces <- FALSE #Ca prend du temps à tourner...
+faire_choix_k_opti <- FALSE
+faire_PCA <- FALSE
 
 ### Ca ça ne bouge pas beaucoup normalement
 
-liste_annees <- 2000:2002
+# liste_annees <- 2000:2002
+liste_annees <- 2017:2018
 
 liste_pays <- c("FR", "ES", "IT", "DE", "PT", "HU")
 
@@ -64,7 +70,9 @@ liste_variables <- c('QHHNUM', #Identifiant ménage
                      'MAINSTAT', #Pour savoir si au foyer #Pour indice précarité
                      'LOOKREAS',
                      'EXIST2J',
-                     'HHPARTNR'
+                     'HHPARTNR',
+                     'SUPVISOR', #Manager
+                     "WSTAT1Y"
                      
 )
 
@@ -76,15 +84,15 @@ planter_si_non_specifie <- FALSE #Plante si toutes les varibales ne sont pas sp�
 
 
 
-liste_cols_cont <- c("Nb_enfants_moins_2_ans","Nb_enfants_entre_3_5_ans", "Age_tranche")
-liste_cols_dummies <- c("Niveau_education", "Temps_partiel", "Pays", "Raisons_temps_partiel", "CSP", "sit_pro_foyer", "statu_marital", "indic_precarite_emp")
+liste_cols_cont <- c("Age_tranche", "Index_conservatisme", "nb_enf_tot")
+liste_cols_dummies <- c("enf_m3ans","Niveau_education", "Temps_partiel", "Pays", "raisons_tp_enf_fam", "sit_pro_foyer", "indic_precarite_emp", "travail_respon")
 liste_cols_to_delete <- c('Identifiant_menage', "Sexe_1H_2F")
 
 
 
 liste_var_sup <- c("Pays_HU", "Pays_DE", "Pays_DK", "Pays_ES", "Pays_FR", "Pays_IT") #Pour le cercle des corrélations
 
-n_sample <- 10000 #La taille de l'échantillon pour la PCA 
+n_sample <- 100 #La taille de l'échantillon pour la PCA 
 inter_max <- 25 #Le nb de fois qu'il itère au maximum
 nb_axes_ACM <- 5
 
@@ -115,62 +123,76 @@ if(creer_base){
 source(paste(repo_prgm , "03_nettoyage.R" , sep = "/"))
 data_merged <- calcul_index_conservatisme(data_merged)
 
+table(data_merged$CSP_tot_1)
+table(data_merged$CSP_tot_2)
+table(data_merged$CSP_TOT)
+
+
+
 ################################################################################
 #            III. PREPARATION DE LA MODELISATION             ===============================
 ################################################################################
 
-data_merged
+# table(data_merged$travail_respon)
 
 source(paste(repo_prgm , "07_preparation_modelisation.R" , sep = "/"))
 
+# 800 000 lignes pour 2000 --> 2002
+# 1 060 000 lignes pour 2017 --> 2018
+# 520 000 lignes pour 2018
 
 ################################################################################
 #            IV. MODELISATION    ===============================================
 ################################################################################
-data_merged
+# data_merged
 
 ################################################################################
 #            LA PCA   ----------------------------------------------------------
 ################################################################################
 
-resultats_acp  <- PCA(data_merged, scale.unit = TRUE, ncp = 5, ind.sup = liste_indices_sup,
-                      quanti.sup=NULL, quali.sup=NULL, graph=FALSE)
-
-get_eigenvalue(resultats_acp)
-fviz_eig(resultats_acp, addlabels = TRUE)
-
-
-fviz_pca_var(resultats_acp,  choix = "var")
-
+if(faire_PCA){
+  resultats_acp  <- PCA(data_merged, scale.unit = TRUE, ncp = 5, ind.sup = liste_indices_sup,
+                        quanti.sup=NULL, quali.sup=NULL, graph=FALSE)
+  
+  get_eigenvalue(resultats_acp)
+  fviz_eig(resultats_acp, addlabels = TRUE)
+  
+  
+  fviz_pca_var(resultats_acp,  choix = "var")
+}
 
 ################################################################################
 #            L'ACM   -----------------------------------------------------------
 ################################################################################
-data_merged_non_encoded
-data_merged_non_encoded[, eval(liste_cols_to_delete) :=NULL] 
-data_merged_non_encoded[is.na(data_merged_non_encoded)] <- 9999
-data_merged_non_encoded
+if(kmeans_sur_proj){
+  data_merged_non_encoded
+  data_merged_non_encoded[, eval(liste_cols_to_delete) :=NULL] 
+  data_merged_non_encoded[is.na(data_merged_non_encoded)] <- 9999
+  data_merged_non_encoded
+  
+  data_merged_non_encoded <- as.data.frame(data_merged_non_encoded)
+  for (i in 1:ncol(data_merged_non_encoded)){
+    data_merged_non_encoded[,i] <- as.factor(data_merged_non_encoded[,i])
+  }
+  
+  acm_results <- dudi.acm(data_merged_non_encoded, nf = nb_axes_ACM, scannf = FALSE)
 
-data_merged_non_encoded <- as.data.frame(data_merged_non_encoded)
-for (i in 1:ncol(data_merged_non_encoded)){
-  data_merged_non_encoded[,i] <- as.factor(data_merged_non_encoded[,i])
+  
+  # A EXECUTER POUR AVOIR DES GRAPHES SUR L'ACM 
+  # fviz_screeplot(acm_results)
+  # summary(acm_results)
+  # inertia.dudi(acm_results)
+  # 
+  # s.corcircle(acm_results$co, 1, 2, clabel = 0.7)
+  # 
+  # fviz_mca_var(acm_results, repel = TRUE) #==> Plus visuel je trouve
+  # 
+  # numero_axe <- 1
+  # fviz_contrib(acm_results, choice = "var", axes = numero_axe)
+  # 
+  # On récupère la projection si on veut essayer les kmeans dessus
+  data_projected <- acm_results$li
 }
-
-acm_results <- dudi.acm(data_merged_non_encoded, nf = nb_axes_ACM, scannf = FALSE)
-
-fviz_screeplot(acm_results)
-summary(acm_results)
-inertia.dudi(acm_results)
-
-s.corcircle(acm_results$co, 1, 2, clabel = 0.7)
-
-fviz_mca_var(acm_results, repel = TRUE) #==> Plus visuel je trouve
-
-numero_axe <- 2
-fviz_contrib(acm_results, choice = "var", axes = numero_axe)
-
-# On récupère la projection si on veut essayer les kmeans dessus
-data_projected <- acm_results$li
 
 ################################################################################
 #            LES KMEANS  -------------------------------------------------------
@@ -206,83 +228,86 @@ data_kmeans_scale <- remove_constant(data_kmeans_scaled, na.rm = FALSE, quiet = 
 
 ############ POUR UNE RAISON QUI M'ECHAPPE LA FONCTION kmeans A L'AIR DE FAIRE DES CLUSTERS PLUS JOLIS############
 
+if(faire_traces){
+  ########## Si votre PC tient le coup : pour trouver le nombre de clusters optimal ""à la main""
+  k2 <- Kmeans(data_kmeans_scale, centers = 2, nstart = 25, method = methode, iter.max=inter_max)
+  k3 <- Kmeans(data_kmeans_scale, centers = 3, nstart = 25, method = methode, iter.max=inter_max)
+  k4 <- Kmeans(data_kmeans_scale, centers = 4, nstart = 25, method = methode, iter.max=inter_max)
+  k5 <- Kmeans(data_kmeans_scale, centers = 5, nstart = 25, method = methode, iter.max=inter_max)
+  
+  # plots to compare
+  p1 <- fviz_cluster(k2, geom = "point", data = data_kmeans_scale) + ggtitle("k = 2")
+  p2 <- fviz_cluster(k3, geom = "point",  data = data_kmeans_scale) + ggtitle("k = 3")
+  p3 <- fviz_cluster(k4, geom = "point",  data = data_kmeans_scale) + ggtitle("k = 4")
+  p4 <- fviz_cluster(k5, geom = "point",  data = data_kmeans_scale) + ggtitle("k = 5")
+  
+  p <- grid.arrange(p1, p2, p3, p4, nrow = 2)
+  
+  
+  nom_graphe <- paste("00_graphe_clustering_", methode,"_kmeans_sur_proj_",kmeans_sur_proj, ".pdf", sep = "")
+  ggsave(paste(repo_sorties, nom_graphe, sep = "/"), p ,  width = 297, height = 210, units = "mm")
+}
 
-########## Si votre PC tient le coup : pour trouver le nombre de clusters optimal ""à la main""
-k2 <- Kmeans(data_kmeans_scale, centers = 2, nstart = 25, method = methode, iter.max=inter_max)
-k3 <- Kmeans(data_kmeans_scale, centers = 3, nstart = 25, method = methode, iter.max=inter_max)
-k4 <- Kmeans(data_kmeans_scale, centers = 4, nstart = 25, method = methode, iter.max=inter_max)
-k5 <- Kmeans(data_kmeans_scale, centers = 5, nstart = 25, method = methode, iter.max=inter_max)
-
-# plots to compare
-p1 <- fviz_cluster(k2, geom = "point", data = data_kmeans_scale) + ggtitle("k = 2")
-p2 <- fviz_cluster(k3, geom = "point",  data = data_kmeans_scale) + ggtitle("k = 3")
-p3 <- fviz_cluster(k4, geom = "point",  data = data_kmeans_scale) + ggtitle("k = 4")
-p4 <- fviz_cluster(k5, geom = "point",  data = data_kmeans_scale) + ggtitle("k = 5")
-
-p <- grid.arrange(p1, p2, p3, p4, nrow = 2)
-
-
-nom_graphe <- paste("00_graphe_clustering_", methode,"_kmeans_sur_proj_",kmeans_sur_proj, ".pdf", sep = "")
-ggsave(paste(repo_sorties, nom_graphe, sep = "/"), p ,  width = 297, height = 210, units = "mm")
-
-####################### VERIFICATION DU NB DE CLUSTERS OPTI ####################### 
-
-# Elbow method
-fviz_nbclust(sample_scaled, Kmeans, method = "wss") +
-  labs(subtitle = "Elbow method")
-# For each value of K, we are calculating WCSS (Within-Cluster Sum of Square). 
-# Kopti = le point d'inflexion ==> Moyen visible ici je trouve (table entière) 
-# Kopti = Idem (projection ACM)
-
-# Silhouette method
-fviz_nbclust(sample_scaled, Kmeans, method = "silhouette")+
-  labs(subtitle = "Silhouette method")
-# In short, the average silhouette approach measures the quality of a clustering. That is, it determines how well each object lies within its cluster. A high average silhouette width indicates a good clustering. The average silhouette method computes the average silhouette of observations for different values of k. The optimal number of clusters k is the one that maximizes the average silhouette over a range of possible values for k.
-## Que les variables de Charlotte :
-# Donne Kopti = 6 (table entière)
-# Kopti = 2 (projection ACM)
-## En ajoutant les variables de Laurie : 
-# Donne Kopti = 4 ou 8 (table entière)? Eventuellement 2
-# Kopti = 2 (projection ACM)
-
-
-# Gap statistic ==> Plus long à tourner attention
-# nboot = 50 to keep the function speedy. 
-# recommended value: nboot= 500 for your analysis.
-# Use verbose = FALSE to hide computing progression.
-set.seed(123)
-fviz_nbclust(sample_scaled, Kmeans, nstart = 25,  method = "gap_stat", nboot = 25)+
-  labs(subtitle = "Gap statistic method")
-# The gap statistic compares the total intracluster variation for different values of k with their expected values under null reference distribution of the data (i.e. a distribution with no obvious clustering)
-## Que les variables de Charlotte :
-# Donne Kopti = 6 (table entière)
-# Kopti = 6 (projection ACM)
-## En ajoutant les variables de Laurie : 
-# Donne Kopti = 4 (table entière), ou de plus en plus...
-# Kopti = 5, 6 ou 7 (projection ACM)
+if(faire_choix_k_opti){
+  ####################### VERIFICATION DU NB DE CLUSTERS OPTI ####################### 
+  
+  # Elbow method
+  fviz_nbclust(sample_scaled, Kmeans, method = "wss") +
+    labs(subtitle = "Elbow method")
+  # For each value of K, we are calculating WCSS (Within-Cluster Sum of Square). 
+  # Kopti = le point d'inflexion ==> Moyen visible ici je trouve (table entière) 
+  # Kopti = Idem (projection ACM)
+  
+  # Silhouette method
+  fviz_nbclust(sample_scaled, Kmeans, method = "silhouette")+
+    labs(subtitle = "Silhouette method")
+  # In short, the average silhouette approach measures the quality of a clustering. That is, it determines how well each object lies within its cluster. A high average silhouette width indicates a good clustering. The average silhouette method computes the average silhouette of observations for different values of k. The optimal number of clusters k is the one that maximizes the average silhouette over a range of possible values for k.
+  
+  ## Que les variables de Charlotte :
+  # Donne Kopti = 6 (table entière)
+  # Kopti = 2 (projection ACM)
+  ## En ajoutant les variables de Laurie : 
+  # Donne Kopti = 4 ou 8 (table entière)? Eventuellement 2
+  # Kopti = 2 (projection ACM)
+  
+  
+  # Gap statistic ==> Plus long à tourner attention
+  # nboot = 50 to keep the function speedy. 
+  # recommended value: nboot= 500 for your analysis.
+  # Use verbose = FALSE to hide computing progression.
+  set.seed(123)
+  fviz_nbclust(sample_scaled, Kmeans, nstart = 25,  method = "gap_stat", nboot = 25)+
+    labs(subtitle = "Gap statistic method")
+  # The gap statistic compares the total intracluster variation for different values of k with their expected values under null reference distribution of the data (i.e. a distribution with no obvious clustering)
+  
+  ## Que les variables de Charlotte :
+  # Donne Kopti = 6 (table entière)
+  # Kopti = 6 (projection ACM)
+  ## En ajoutant les variables de Laurie : 
+  # Donne Kopti = 4 (table entière), ou de plus en plus...
+  # Kopti = 5, 6 ou 7 (projection ACM)
+}
 
 ####################### Une fois qu'on a trouvé le nb de clusters ####################### 
-nb_clusters <- 6
-
 
 # Sur le sample_scaled
-sample_scaled
-resKM_sample_scaled <- Kmeans(sample_scaled, centers=nb_clusters, nstart=30, method = methode)
-resKM_sample_scaled
-fviz_cluster(resKM_sample_scaled, sample_scaled)
+# sample_scaled
+# resKM_sample_scaled <- Kmeans(sample_scaled, centers=nb_clusters, nstart=30, method = methode)
+# resKM_sample_scaled
+# fviz_cluster(resKM_sample_scaled, sample_scaled)
 
 # summary(resKM_sample_scaled)
 # plot(sample_scaled, col = resKM_sample_scaled$cluster,pch=16,cex=1.2,main="Regroupement par les k-means")
 
 
+resKM <- Kmeans(data_kmeans_scale, centers=nb_clusters, nstart=25, method = methode)
 
-# Sur toute la table
-resKM <- Kmeans(data_kmeans_scale, centers=nb_clusters, nstart=30, method = methode)
-resKM
-p <- fviz_cluster(resKM, data_kmeans_scale)
-nom_graphe <- paste("01_graphe_", methode,"_",nb_clusters, "_clusters","_kmeans_sur_proj_",kmeans_sur_proj, ".pdf", sep = "")
-ggsave(paste(repo_sorties, nom_graphe, sep = "/"), p ,  width = 297, height = 210, units = "mm")
-
+if(faire_traces){
+  # Sur toute la table
+  p <- fviz_cluster(resKM, data_kmeans_scale)
+  nom_graphe <- paste("01_graphe_", methode,"_",nb_clusters, "_clusters","_kmeans_sur_proj_",kmeans_sur_proj, ".pdf", sep = "")
+  ggsave(paste(repo_sorties, nom_graphe, sep = "/"), p ,  width = 297, height = 210, units = "mm")
+}
 
 ################################################################################
 ########## ANALYSE DU CLUSTERING ###############################################
@@ -290,17 +315,17 @@ ggsave(paste(repo_sorties, nom_graphe, sep = "/"), p ,  width = 297, height = 21
 source(paste(repo_prgm , "08_analyse_modelisation.R" , sep = "/"))
 
 
-df_analyse_cluster_sample_scaled <- fonction_calcul_scoring_kmeans(sample_scaled, resKM_sample_scaled)
+# df_analyse_cluster_sample_scaled <- fonction_calcul_scoring_kmeans(sample_scaled, resKM_sample_scaled)
 df_analyse_cluster <- fonction_calcul_scoring_kmeans(data_kmeans_scale, resKM)
 
-df_analyse_cluster_sample_scaled
-df_analyse_cluster
-summary(df_analyse_cluster)
+# df_analyse_cluster_sample_scaled
+# df_analyse_cluster
+# summary(df_analyse_cluster)
 titre <- paste("Analyse_cluster_score_", methode,"_",nb_clusters, "_clusters","_kmeans_sur_proj_",kmeans_sur_proj, ".xlsx", sep = "")
 write.xlsx(df_analyse_cluster, paste(repo_sorties,titre, sep = "/"))
 
 
-100*resKM_sample_scaled$size/nrow(sample_scaled) ## La taille des clusters en % de l'effectif (sample_scaled)
+# 100*resKM_sample_scaled$size/nrow(sample_scaled) ## La taille des clusters en % de l'effectif (sample_scaled)
 100*resKM$size/nrow(data_kmeans_scale) ## La taille des clusters en % de l'effectif
 
 
@@ -311,7 +336,7 @@ data_merged_copy <- copy(data_merged)
 
 # On ajoute la prédiction du numéro de cluster
 data_merged_copy$clustering <- resKM$cluster
-sample$clustering <- resKM_sample_scaled$cluster
+# sample$clustering <- resKM_sample_scaled$cluster
 
 
 
@@ -362,60 +387,60 @@ for (variable in liste_cols_dummies[c(-1)]){
 counted_occurences_all
 titre <- paste("Analyse_cluster_contenu_", methode,"_",nb_clusters, "_clusters","_kmeans_sur_proj_",kmeans_sur_proj, ".xlsx", sep = "")
 write.xlsx(counted_occurences_all, paste(repo_sorties,titre, sep = "/"))
-# write.xlsx(counted_occurences_all, paste(repo_sorties,"Analyse_cluster_contenu_cat_0.xlsx", sep = "/"))
 
 
 
-## Pour tout regarder
-# Option 1 : 
-describeBy(data_merged_copy, group="clustering")
-
-# Option 2 : 
-data_merged_copy %>% 
-  split(.$clustering) %>% 
-  map(summary)
-
-
+# 
+# ## Pour tout regarder
+# # Option 1 : 
+# describeBy(data_merged_copy, group="clustering")
+# 
+# # Option 2 : 
+# data_merged_copy %>% 
+#   split(.$clustering) %>% 
+#   map(summary)
+# 
+# 
 ############### ESSAI DE LA DISTANCE GOWER ##################
-
-
-data_merged_non_encoded
-
-data_merged_non_encoded_sample <- as.data.table(sapply(data_merged_non_encoded[], sample, n_sample))
-data_merged_non_encoded_sample <- as.data.frame(data_merged_non_encoded_sample)
-for (i in 1:ncol(data_merged_non_encoded_sample)){
-  data_merged_non_encoded_sample[,i] <- as.factor(data_merged_non_encoded_sample[,i])
-}
-
-gower_dist <- daisy(data_merged_non_encoded_sample,
-                    metric = "gower")
-
-summary(gower_dist)
-
-gower_mat <- as.matrix(gower_dist)
-
-
-# Choix nb clusters Kopti
-sil_width <- c(NA)
-for(i in 2:10){
-  pam_fit <- pam(gower_dist,
-                 diss = TRUE,
-                 k = i)
-  sil_width[i] <- pam_fit$silinfo$avg.width
-  
-}
-
-# Plot sihouette width (higher is better)
-plot(1:10, sil_width,
-     xlab = "Number of clusters",
-     ylab = "Silhouette Width")
-lines(1:10, sil_width)
-
-nb_clusters <- 4
-
-pam_fit <- pam(gower_dist, diss = TRUE, k = nb_clusters)
-
-pam_fit$clustering
+# 
+# 
+# data_merged_non_encoded
+# 
+# data_merged_non_encoded_sample <- as.data.table(sapply(data_merged_non_encoded[], sample, n_sample))
+# data_merged_non_encoded_sample <- as.data.frame(data_merged_non_encoded_sample)
+# for (i in 1:ncol(data_merged_non_encoded_sample)){
+#   data_merged_non_encoded_sample[,i] <- as.factor(data_merged_non_encoded_sample[,i])
+# }
+# 
+# gower_dist <- daisy(data_merged_non_encoded_sample,
+#                     metric = "gower")
+# 
+# summary(gower_dist)
+# 
+# gower_mat <- as.matrix(gower_dist)
+# 
+# 
+# # Choix nb clusters Kopti
+# sil_width <- c(NA)
+# for(i in 2:10){
+#   pam_fit <- pam(gower_dist,
+#                  diss = TRUE,
+#                  k = i)
+#   sil_width[i] <- pam_fit$silinfo$avg.width
+#   
+# }
+# 
+# # Plot sihouette width (higher is better)
+# plot(1:10, sil_width,
+#      xlab = "Number of clusters",
+#      ylab = "Silhouette Width")
+# lines(1:10, sil_width)
+# 
+# nb_clusters <- 4
+# 
+# pam_fit <- pam(gower_dist, diss = TRUE, k = nb_clusters)
+# 
+# pam_fit$clustering
 
 # ################################################################################
 # ##### BROUILLON : CLUSTERING GAP ###############################################
